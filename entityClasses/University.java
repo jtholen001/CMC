0c32282f9530a257af3fac84262fcd59472501dc
BKALSOW001 <bkalsow001@vplincsci007.ad.csbsju.edu>
2018-02-20 21:38:56 -0600

    University Commit

---
+++ entityClasses/University.java
@@ -0,0 +1,10 @@
+/**
+ * This class represents a University
+ * @Author Michael Carroll, Brandan Kalsow, Nicholas Tawil, Jordan Tholen, Ryan Strelow
+ * @Version 2/20/2020
+ */
+
+public class University
+{
+  
+}
