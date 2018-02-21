bf9a84ecadd3809e9b9007b8add06dee63f7279e
BKALSOW001 <bkalsow001@csbsju.edu>
2018-02-21 15:33:29 -0600

    University update 1

--- entityClasses/University.java
+++ entityClasses/University.java
@@ -3,8 +3,24 @@
  * @Author Michael Carroll, Brandan Kalsow, Nicholas Tawil, Jordan Tholen, Ryan Strelow
  * @Version 2/20/2020
  */
-
+import java.util.ArrayList;
 public class University
 {
-  sgsdghsdghdes
+  private String name;
+  private String state;
+  private String location;
+  private String control;
+  private int numStudents;
+  private int percentFemale;
+  private int SATVerbal;
+  private int SATMath;
+  private int expenses;
+  private double percentFinancialAid;
+  private int numApplicants;
+  private double percentAdmitted;
+  private double percentEnrolled;
+  private int academicScale;
+  private int socialScale;
+  private int qualityOfLifeScale;
+  private ArrayList<String> emphases;
 }
