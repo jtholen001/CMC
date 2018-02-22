cb063f31424107fd53a47831545ff0a1f7d9d9d5
NTAWIL001 <ntawil98@gmail.com>
2018-02-21 19:47:23 -0600

    Changed DB controller and added the users controller

--- Controllers/UniversityController.java
+++ Controllers/UniversityController.java
@@ -1,30 +1,44 @@
 /**
  * A controller class that handles University functionalities
  * 
- * @author Nicholas Tawil
+ *  @author Michael Carroll, Nicholas Tawil, Brandan Kalsow, Jordan Tholen, Ryan Strelow
  * @version 2/20/18
  */
 public class UniversityController
 {
   /**
+   * an instance of the DBController
+   */
+  private DBController dbCont;
+  
+  /**
    * Constructor for a UniversityController object
    */
-  public UniversityController(){}
+  public UniversityController()
+  {
+    this.dbCont = new DBController();
+  }
   
   /**
    * A method that displays all University objects in the Database
    */
-  public void viewUniversities(){}
+  public void viewUniversities()
+  {
+  }
   
   /**
    * A method that allows an Admin to edit a University
    * 
    * @param university the University object to be edited
    */
-  public void editUniversity(University university){}
+  public void editUniversity(University university)
+  {
+  }
   
   /**
    * A method that allows an Admin to add a University
    */
-  public void addUniversity(){}
+  public void addUniversity()
+  {
+  }
 }
--- Controllers/UsersController.java
+++ Controllers/UsersController.java
@@ -1,17 +1,61 @@
 /**
  * A controller class 1manages a User
  * 
- * @author Nicholas Tawil
+ *  @author Michael Carroll, Nicholas Tawil, Brandan Kalsow, Jordan Tholen, Ryan Strelow
  * @version 2/20/18
  */
 public class UsersController
 {
   /**
-   * Constuctor for UsersController
+   * an array of Users
    */
-  public UsersController(){}
+  private User[] users;
   
   /**
-   * 
+   * an instance of a DBController
    */
+  private DBController dbCont;
+  
+  /**
+   * Constuctor for UsersController
+   */
+  public UsersController()
+  {
+    this.dbCont = new DBController();
+  }
+  
+  /**
+   * a method to view all Users in the Database
+   */
+  public void viewUsers()
+  {
+    
+  }
+  
+  /**
+   * a method to edit a single User
+   * 
+   * @param username a string representing the username of a User
+   */
+  public void editUser(String username)
+  {
+    
+  }
+  
+  /**
+   * a method to add a User to the Database
+   */
+  public void addUser()
+  {
+    
+  }
+  
+  /**
+   * a method to deactivate a User
+   * 
+   * @param user a User object to be deactivated
+   */
+  public void deactivate(User user)
+  {
+  }
 }
