90c5a2479e9f26546d21e5fc6edca94dd8c98e25
JTHOLEN001 <jordantholen8@gmail.com>
2018-02-21 20:46:18 -0600

    added the empty profile controller

--- Controllers/ProfileController.java
+++ Controllers/ProfileController.java
@@ -1,6 +1,28 @@
 public class ProfileController
 {
-  private DatabaseController
+  //Student variable to edit
+  DBController dbc;
+  
+  // Constructor
+  /** 
+   * Construct a Profile controller with the given student.
+   * 
+   * @param queueName the name of this queue
+   */
+  public ProfileController()
+  {
+    dbc = new DBController();
+  }
+  
+  /** 
+   * method to edit the profile of the given student
+   * 
+   * @param Student the name of this student
+   */
+  public void editProfile(Student student)
+  {
+    
+  }
   
   
 }
