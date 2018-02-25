e4f1bffd9a0d415757623adb625a32052d9bd495
MCARROLL001 <mcarroll001@csbsju.edu>
2018-02-25 14:47:58 -0600

    added viewProfile and toString() methods

--- Controllers/ProfileController.java
+++ Controllers/ProfileController.java
@@ -2,27 +2,30 @@ public class ProfileController
 {
   //Student variable to edit
   DBController dbc;
-  
+
   // Constructor
-  /** 
+  /**
    * Construct a Profile controller with the given student.
-   * 
+   *
    * @param queueName the name of this queue
    */
   public ProfileController()
   {
     dbc = new DBController();
   }
-  
-  /** 
+
+  /**
    * method to edit the profile of the given student
-   * 
+   *
    * @param Student the name of this student
    */
   public void editProfile(Student student)
   {
     
   }
-  
-  
+
+  public void viewProfile(Student student)
+  {
+    return student.toString();
+  }
 }
