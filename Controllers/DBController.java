f8847c846a379bd309a6a6feae304b7c1d0f4ca7
BKALSOW001 <bkalsow001@csbsju.edu>
2018-02-25 15:38:34 -0600

    added getUniversity and addUniversity to DBController

--- Controllers/DBController.java
+++ Controllers/DBController.java
@@ -1,127 +1,152 @@
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DBController
 {
-  /**
-   * Construct a database controller
+  /** 
+   * Construct a database controller 
    */
   public DBController()
   {
   }
-
-  /**
+  
+  /** 
    * method to get a specific user based off of the given name
-   *
+   * 
    * @param username the username of the user
-   *
+   * 
    * @return a user object
    */
   public User getUser(String username)
   {
     return null;
   }
-
-  /**
+  
+  /** 
    * method to get all users from the database
-   *
+   * 
    * @return an ArrayList of all users
    */
   public ArrayList<User> getUsers()
   {
     return null;
   }
-
-  /**
+  
+  /** 
    * method to save an edited user to the database
    */
-  public void saveEditedUser(User u)
+  public void saveEditedUser()
   {
-
+    
   }
-
-  /**
+  
+  /** 
    * method to add a user to the database
-   *
+   * 
    * @param User to add to the database
    */
   public void addUser(User u)
   {
 
   }
-
-  /**
+  
+  /** 
    * method to get all universities in the databse
-   *
+   * 
    * @return an ArrayList of all universities
    */
   public ArrayList<University> viewUniversities()
   {
     return null;
   }
-
-  /**
+  
+  /** 
    * method to get a specific university off of the name
-   *
+   * 
    * @param a string of the university name
-   *
+   * 
    * @return a University object
    */
   public University getUniversity(String name)
   {
-    return null;
+    String[][] universities = university_getUniversities();
+    HashMap<String, University> universityMap = new HashMap<String, University>();
+    
+    for(int index = 0; index < universities.length; index++)
+    {
+      universityMap.put(universities[index][0], new University(universities[index][0], universities[index][1],
+                                                               universities[index][2],universities[index][3], 
+                                                               universities[index][4], universities[index][5],
+                                         universities[index][6], universities[index][7], universities[index][8],
+                                         universities[index][9], universities[index][10],universities[index][11],
+                                         universities[index][12], universities[index][13], universities[index][14],
+                                         universities[index][15], null)); //not sure how emphases are stored
+    }
+    
+    return universityMap.get(name);
+    
+    
+    
   }
-
-  /**
+  
+  /** 
    * method to to save an editied university
-   *
+   * 
    * @param university the university object to edit
    */
   public void saveEditedUniversity(University university)
   {
-    university_editUniversity(university.getName(), university.getState(), university.getLocation(),
+    university_editUniversity(university.getName(), university.getState(), university.getLocation(), 
                               university.getControl(), university.getNumStudents(), university.getPercentFemale(),
                               university.getSATVerbal(), university.getSATMath(), university.getExpenses(),
                               university.getPercentFinancialAid(), university.getNumApplicants(),
                               university.percentAdmitted(), university.getPercentEnrolled(),
                               university.getAcademicScale(), university.getSocialScale(),
-                              university.getQualityOfLifeScale(), university.getEmphases());
+                              university.getQualityOfLifeScale());
   }
-
-  /**
+  
+  /** 
    * method to add a university to the database
-   *
+   * 
    * @param a University object to add
    */
-  public void addUniversity(University u)
+  public void addUniversity(University university)
   {
-
+    university_addUniversity(university.getName(), university.getState(), university.getLocation(), 
+                              university.getControl(), university.getNumStudents(), university.getPercentFemale(),
+                              university.getSATVerbal(), university.getSATMath(), university.getExpenses(),
+                              university.getPercentFinancialAid(), university.getNumApplicants(),
+                              university.percentAdmitted(), university.getPercentEnrolled(),
+                              university.getAcademicScale(), university.getSocialScale(),
+                              university.getQualityOfLifeScale());
   }
-
-  /**
+  
+  /** 
    * method to remove a university from a specific student
-   *
+   * 
    * @param a Student object to remove from
    * @param a University object to remove from the student
-   *
+   * 
    */
   public void removeUniversityFromStudent(Student s, University u)
   {
-
+    
   }
-
-  /**
+    
+  /** 
    * method to save an edited university
-   *
+   * 
    * @param a university object to save
    */
   public void saveUniversity(University u)
   {
-
+    
   }
-
-  /**
+  
+  /** 
    * method to search all universities and return the mataching ones
-   *
+   * 
    * @param name the name of this university as a String
    * @param state the state this university is located in as a String
    * @param location whether the university is located in and urban or rural setting
@@ -139,30 +164,30 @@ public class DBController
    * @param socialScale how this university rates socially
    * @param qualityOfLifeScale the quality of life at this university
    * @param emphases what areas of study this university emphasizes
-   *
+   * 
    * @return an ArrayList of all universities that match the criteria
    */
-  public ArrayList<University> searchUniversities(String name,String state,String location, String control, int numStudents,
-                    int percentFemale, int SATVerbal, int SATMath, int expenses, double percentFinancialAid,
+  public ArrayList<University> searchUniversities(String name,String state,String location, String control, int numStudents, 
+                    int percentFemale, int SATVerbal, int SATMath, int expenses, double percentFinancialAid, 
                     int numApplicants, double percentAdmitted, double percentEnrolled, int academicScale, int socialScale,
                     int qualityOfLifeScale, ArrayList<String> emphases)
   {
     return null;
   }
-
-
-  /**
+  
+  
+  /** 
    * method to get the recommended universities based off of a university
-   *
+   * 
    * @param a university object to base the other schools off of
-   *
+   * 
    * @return an ArrayList of univerity objects
    */
   public ArrayList<University> getRecommendedUniversities(University u)
   {
     return null;
   }
-
-
-
+  
+  
+  
 }
