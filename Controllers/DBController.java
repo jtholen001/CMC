5f38283126d2a3ce54358b487dad4cfad80ee8a0
MCARROLL001 <mcarroll001@csbsju.edu>
2018-02-25 15:34:03 -0600

    edited parameters in DBController.java

--- Controllers/DBController.java
+++ Controllers/DBController.java
@@ -2,83 +2,83 @@ import java.util.ArrayList;
 
 public class DBController
 {
-  /** 
-   * Construct a database controller 
+  /**
+   * Construct a database controller
    */
   public DBController()
   {
   }
-  
-  /** 
+
+  /**
    * method to get a specific user based off of the given name
-   * 
+   *
    * @param username the username of the user
-   * 
+   *
    * @return a user object
    */
   public User getUser(String username)
   {
     return null;
   }
-  
-  /** 
+
+  /**
    * method to get all users from the database
-   * 
+   *
    * @return an ArrayList of all users
    */
   public ArrayList<User> getUsers()
   {
     return null;
   }
-  
-  /** 
+
+  /**
    * method to save an edited user to the database
    */
-  public void saveEditedUser()
+  public void saveEditedUser(User u)
   {
-    
+
   }
-  
-  /** 
+
+  /**
    * method to add a user to the database
-   * 
+   *
    * @param User to add to the database
    */
   public void addUser(User u)
   {
 
   }
-  
-  /** 
+
+  /**
    * method to get all universities in the databse
-   * 
+   *
    * @return an ArrayList of all universities
    */
   public ArrayList<University> viewUniversities()
   {
     return null;
   }
-  
-  /** 
+
+  /**
    * method to get a specific university off of the name
-   * 
+   *
    * @param a string of the university name
-   * 
+   *
    * @return a University object
    */
   public University getUniversity(String name)
   {
     return null;
   }
-  
-  /** 
+
+  /**
    * method to to save an editied university
-   * 
+   *
    * @param university the university object to edit
    */
   public void saveEditedUniversity(University university)
   {
-    university_editUniversity(university.getName(), university.getState(), university.getLocation(), 
+    university_editUniversity(university.getName(), university.getState(), university.getLocation(),
                               university.getControl(), university.getNumStudents(), university.getPercentFemale(),
                               university.getSATVerbal(), university.getSATMath(), university.getExpenses(),
                               university.getPercentFinancialAid(), university.getNumApplicants(),
@@ -86,42 +86,42 @@ public class DBController
                               university.getAcademicScale(), university.getSocialScale(),
                               university.getQualityOfLifeScale(), university.getEmphases());
   }
-  
-  /** 
+
+  /**
    * method to add a university to the database
-   * 
+   *
    * @param a University object to add
    */
   public void addUniversity(University u)
   {
-    
+
   }
-  
-  /** 
+
+  /**
    * method to remove a university from a specific student
-   * 
+   *
    * @param a Student object to remove from
    * @param a University object to remove from the student
-   * 
+   *
    */
   public void removeUniversityFromStudent(Student s, University u)
   {
-    
+
   }
-    
-  /** 
+
+  /**
    * method to save an edited university
-   * 
+   *
    * @param a university object to save
    */
   public void saveUniversity(University u)
   {
-    
+
   }
-  
-  /** 
+
+  /**
    * method to search all universities and return the mataching ones
-   * 
+   *
    * @param name the name of this university as a String
    * @param state the state this university is located in as a String
    * @param location whether the university is located in and urban or rural setting
@@ -139,30 +139,30 @@ public class DBController
    * @param socialScale how this university rates socially
    * @param qualityOfLifeScale the quality of life at this university
    * @param emphases what areas of study this university emphasizes
-   * 
+   *
    * @return an ArrayList of all universities that match the criteria
    */
-  public ArrayList<University> searchUniversities(String name,String state,String location, String control, int numStudents, 
-                    int percentFemale, int SATVerbal, int SATMath, int expenses, double percentFinancialAid, 
+  public ArrayList<University> searchUniversities(String name,String state,String location, String control, int numStudents,
+                    int percentFemale, int SATVerbal, int SATMath, int expenses, double percentFinancialAid,
                     int numApplicants, double percentAdmitted, double percentEnrolled, int academicScale, int socialScale,
                     int qualityOfLifeScale, ArrayList<String> emphases)
   {
     return null;
   }
-  
-  
-  /** 
+
+
+  /**
    * method to get the recommended universities based off of a university
-   * 
+   *
    * @param a university object to base the other schools off of
-   * 
+   *
    * @return an ArrayList of univerity objects
    */
   public ArrayList<University> getRecommendedUniversities(University u)
   {
     return null;
   }
-  
-  
-  
+
+
+
 }
