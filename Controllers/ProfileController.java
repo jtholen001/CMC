c6da6184cac57fa84b50552e70565fb8c5dac76a
MCARROLL001 <mcarroll001@csbsju.edu>
2018-02-26 19:17:58 -0600

    added javadoc

--- Controllers/ProfileController.java
+++ Controllers/ProfileController.java
@@ -6,7 +6,6 @@ public class ProfileController
   // Constructor
   /**
    * Construct a Profile controller with the given student.
-   *
    * @param queueName the name of this queue
    */
   public ProfileController()
@@ -16,7 +15,6 @@ public class ProfileController
 
   /**
    * method to edit the profile of the given student
-   *
    * @param Student the name of this student
    */
   public void editProfile(Student student)
@@ -28,6 +26,10 @@ public class ProfileController
     dbc.saveEditedUser(student);
   }
 
+  /**
+   * method to view the profile of the given student
+   * @param Student the name of this student
+   */
   public void viewProfile(Student student)
   {
     System.out.println(student.toString());
