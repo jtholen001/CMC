7990f1fe73acedda10a1ea6efc82b407135b084f
RSTRELOW001 <rstrelow001@csbsju.edu>
2018-03-12 20:11:53 -0500

    I tried

--- drivers/Driver.java
+++ drivers/Driver.java
@@ -2,21 +2,49 @@
  * 
  */
 package drivers;
+import Interfaces.*;
+import entityClasses.*;
+import Controllers.*;
 
 /**
  * @author @author Michael Carroll, Nicholas Tawil, Brandan Kalsow, Jordan Tholen, Ryan Strelow
  *
  */
 public class Driver {
+	
+	public StudentInterface studentInt;
+	public AdminInterface adminInt;
+	public UserInterface userInt;
+	public Admin admin = new Admin("John", "Miller", "jmiller", "password0", 'a', true, true);
+	public Student student;
 
+	
+	/**
+	 * 
+	 */
+	public static void testLogin() {
+		boolean status = userInt.login("johnmiller", "")
+		
+	}
+	
+	public void createUser() {
+		
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) 
 	{
+		userInt = new UserInterface();
+		
+		
+		studentInt = new StudentInterface(null);
 		// TODO Auto-generated method stub
 		
+		
 	}
+	
 	
 	
 
