63d1719d01cffe97da785407e231c1e8c5b2adbc
RSTRELOW001 <rstrelow001@csbsju.edu>
2018-03-12 20:38:04 -0500

    beginning of the driver

--- drivers/Driver.java
+++ drivers/Driver.java
@@ -5,6 +5,7 @@ package drivers;
 import Interfaces.*;
 import entityClasses.*;
 import Controllers.*;
+import java.util.ArrayList;
 
 /**
  * @author @author Michael Carroll, Nicholas Tawil, Brandan Kalsow, Jordan Tholen, Ryan Strelow
@@ -15,26 +16,59 @@ public class Driver {
 	public StudentInterface studentInt;
 	public AdminInterface adminInt;
 	public UserInterface userInt;
-	public Admin admin = new Admin("John", "Miller", "jmiller", "password0", 'a', true, true);
+	public Admin admin;
 	public Student student;
 
 	public Driver() {
+		student = new Student("John", "User", "juser", "user", 'u', true, false, new ArrayList<University>());
+		admin = new Admin("Noreen", "Admin", "nadmin", "admin", 'a', true, false);
 		userInt = new UserInterface();
-		student = new StudentInterface(null);
+		studentInt = new StudentInterface(student);
+		adminInt = new AdminInterface(admin);
 	}
 	
 	/**
-	 * 
+	 * U1 AND U20
 	 */
-	public static void testLogin() {
+	public void login() {
 		boolean status = userInt.login("johnmiller", "")
 		
 	}
 	
-	public void createUser() {
+	public void searchForUniversities() {
+		boolean status = userInt.login("johnmiller", "")
+		
+	}	
+	//U6 and U7 and U10 maybe U11
+	public void viewMatchedSchools() {
+		boolean status = userInt.login("johnmiller", "")
+		
+	}	
+	
+	//U4 and U7 probably U8
+	public void viewSavedSchools() {
+		boolean status = userInt.login("johnmiller", "")
 		
 	}
 	
+	//U5 and U9
+	public void viewMyProfile() {
+
+		
+	}
+	
+	//U13 and U17, U18, U19
+	public void viewUsers() {
+		
+	}
+	
+	//U14 and U15 and U16
+	public void viewUniversities() {
+
+		
+	}
+	
+
 	/**
 	 * @param args
 	 */
