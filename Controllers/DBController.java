f5d5a54e50e2b85a46006d832900f5833c882c08
NTAWIL001 <ntawil98@gmail.com>
2018-03-27 20:48:33 -0500

    adminInterfaceTest

--- Controllers/DBController.java
+++ Controllers/DBController.java
@@ -139,7 +139,6 @@ public class DBController
       }
     }
     //returns -1 if an error is encountered
-    System.out.println("DBCont: " + user.getLoggedInStatus());
     return univDBlib.user_editUser(user.getUsername(),user.getFirstName(),user.getLastName(),user.getPassword(),user.getType(),
                             temp);
   }
