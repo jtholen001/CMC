c7226edc24e676b42ae42c32e496498e3d355f68
MCARROLL001 <mcarroll001@csbsju.edu>
2018-03-27 21:00:44 -0500

    added exception to Admin entity, modified test class

--- Test/EntityTests/AdminTest.java
+++ Test/EntityTests/AdminTest.java
@@ -4,19 +4,32 @@ import entityClasses.*;
 
 public class AdminTest {
 	@Test
-	public void constructorSucseedsForValidInput()
-	{
-		Assert.assertTrue("Constructor should succeed for university as all fields meet constructor criteria", new Admin("John","Miller","jmill", "password0",
+	public void constructorSucseedsForValidInput() {
+		Assert.assertTrue("Constructor should succeed for admin as all fields meet constructor criteria", new Admin("John","Miller","jmill", "password0",
                 'a', true, false) instanceof Admin);
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void constructorFailsForInvalidInput()
-	{
-		Assert.assertTrue("Constructor fails for university with name null:", new Admin(null, "miller","jmill", "password0",
+	public void constructorFailsForInvalidFirstName() {
+		Assert.assertTrue("Constructor fails for admin with empty firstName parameter:", new Admin("", "miller","jmill", "password0",
                 'a', true, false) instanceof Admin);
-		Assert.assertTrue("Constructor fails for university with state field missing:", new Admin("John", null,"jmill", "password0",
+}
+
+	@Test (expected = IllegalArgumentException.class)
+	public void constructorFailsForInvalidLastName() {
+		Assert.assertTrue("Constructor fails for admin with empty lastName parameter:", new Admin("John", "","jmill", "password0",
                 'a', true, false) instanceof Admin);
-		Assert.assertTrue("Constructor suceeds for university with all fields meeting criteria:", new Admin("John","Miller", null, "password0",
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void constructorFailsForInvalidUsername() {
+		Assert.assertTrue("Constructor fails for admin with empty username parameter:", new Admin("John","Miller", "", "password0",
                 'a', true, false) instanceof Admin);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void constructorFailsForInvalidPassword() {
+		Assert.assertTrue("Constructor fails for admin with empty password parameter:", new Admin("John","Miller", "jmill", "",
+                'a', true, false) instanceof Admin);
+	}
 }
