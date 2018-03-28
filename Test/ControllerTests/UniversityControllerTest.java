e55c17344daf1c92a16a1846d7ab6d27c75f8983
NTAWIL001 <ntawil98@gmail.com>
2018-03-27 22:01:14 -0500

    UniversityControllerTest editUniversitytest

--- Test/ControllerTests/UniversityControllerTest.java
+++ Test/ControllerTests/UniversityControllerTest.java
@@ -26,25 +26,23 @@ public class UniversityControllerTest
 	@Test
 	public void testEditUniversity()
 	{
-		uniCont.editUniversity(university, "MINNESOTA", "CITY", "PRIVATE", 4, 20.0, 100.0, 100.0, 80.0, 1.0, 6, 80.0, 60.0, 2, 2, 2, new ArrayList<String>());
-		Assert.assertTrue("STATE: MINNESOTA", dbCont.getUniversity("UNIVERSITY OF CMC").getState().equals("MINNESOTA"));
-		Assert.assertTrue("Location: CITY", dbCont.getUniversity("UNIVERSITY OF CMC").getLocation().equals("CITY"));
-		Assert.assertTrue("Control: PRIVATE", dbCont.getUniversity("UNIVERSITY OF CMC").getControl().equals("PRIVATE"));
-		Assert.assertTrue("NumStudents : 4", dbCont.getUniversity("UNIVERSITY OF CMC").getNumStudents() == 4);
-		Assert.assertTrue("PercentFemale : 20.0", dbCont.getUniversity("UNIVERSITY OF CMC").getPercentFemale() == 20.0);
-		Assert.assertTrue("SATVerbal : 100.0", dbCont.getUniversity("UNIVERSITY OF CMC").getSATVerbal() == 100.0);
-		Assert.assertTrue("SATMath : 100.0", dbCont.getUniversity("UNIVERSITY OF CMC").getSATMath() == 100.0);
-		Assert.assertTrue("Expenses : 1.0", dbCont.getUniversity("UNIVERSITY OF CMC").getExpenses() == 80.0);
-		Assert.assertTrue("PercentFinancialAid : 80.0", dbCont.getUniversity("UNIVERSITY OF CMC").getPercentFinancialAid() == 1.0);
-		Assert.assertTrue("NumApplicants : ", dbCont.getUniversity("UNIVERSITY OF CMC").getNumApplicants() == 6);
-		Assert.assertTrue("PercentAdmitted : 80.0", dbCont.getUniversity("UNIVERSITY OF CMC").getPercentAdmitted() == 80.0);
-		Assert.assertTrue("PercentEnrolled : 60.0", dbCont.getUniversity("UNIVERSITY OF CMC").getPercentEnrolled() == 60.0);
-		Assert.assertTrue("AcademicScale : 2", dbCont.getUniversity("UNIVERSITY OF CMC").getAcademicScale() == 2);
-		Assert.assertTrue("SocialScale : 2", dbCont.getUniversity("UNIVERSITY OF CMC").getSocialScale() == 2);
-		Assert.assertTrue("QualityOfLifeScale : 2", dbCont.getUniversity("UNIVERSITY OF CMC").getQualityOfLifeScale() == 2);
-		
-		
-		
+		uniCont.editUniversity(university, "MINNESOTA", "CITY", "PRIVATE", 4, 20.0, 100.0, 100.0, 80.0, 1.0, 6, 60.0, 60.0, 2, 2, 2, new ArrayList<String>());
+		University temp = dbCont.getUniversity("UNIVERSITY OF CMC");
+		Assert.assertTrue("STATE: MINNESOTA", temp.getState().equals("MINNESOTA"));
+		Assert.assertTrue("Location: CITY", temp.getLocation().equals("CITY"));
+		Assert.assertTrue("Control: PRIVATE", temp.getControl().equals("PRIVATE"));
+		Assert.assertTrue("NumStudents : 4", temp.getNumStudents() == 4);
+		Assert.assertTrue("PercentFemale : 20.0", temp.getPercentFemale() == 20.0);
+		Assert.assertTrue("SATVerbal : 100.0", temp.getSATVerbal() == 100.0);
+		Assert.assertTrue("SATMath : 100.0", temp.getSATMath() == 100.0);
+		Assert.assertTrue("Expenses : 1.0", temp.getExpenses() == 80.0);
+		Assert.assertTrue("PercentFinancialAid : 80.0", temp.getPercentFinancialAid() == 1.0);
+		Assert.assertTrue("NumApplicants : ", temp.getNumApplicants() == 6);
+		Assert.assertTrue("PercentAdmitted : 80.0", temp.getPercentAdmitted() == 60.0);
+		Assert.assertTrue("PercentEnrolled : 60.0", temp.getPercentEnrolled() == 60.0);
+		Assert.assertTrue("AcademicScale : 2", temp.getAcademicScale() == 2);
+		Assert.assertTrue("SocialScale : 2", temp.getSocialScale() == 2);
+		Assert.assertTrue("QualityOfLifeScale : 2", temp.getQualityOfLifeScale() == 2);	
 	}
 	
 	@After
