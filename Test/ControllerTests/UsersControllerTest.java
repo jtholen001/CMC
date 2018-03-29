be00d247b6a2be4b74e53ebb7dfc235b03c17ba2
BKALSOW001 <bkalsow001@csbsju.edu>
2018-03-28 20:21:58 -0500

    Added more tests to UsersControllerTest

--- Test/ControllerTests/UsersControllerTest.java
+++ Test/ControllerTests/UsersControllerTest.java
@@ -1,9 +1,9 @@
 package Test.ControllerTests;
 
 import org.junit.*;
-import java.util.HashMap;
+import java.util.*;
 import Controllers.UsersController;
-import entityClasses.User;
+import entityClasses.*;
 
 public class UsersControllerTest 
 {
@@ -37,6 +37,45 @@ public class UsersControllerTest
 		HashMap<String, User> users = uCont.viewUsers();
 		Assert.assertTrue("List of users should contain key bkalsow", users.containsKey("bkalsow"));
 		Assert.assertTrue("List of users should contain key nlong", users.containsKey("nlong"));
+	}
+	
+	@Test
+	public void editUserSucceedsForValidParams()
+	{
+		Student student = new Student("Brian", "Coleslaw", "bkalsow", "abcde", 'u', false, false, new ArrayList<University>());
+		uCont.editUser("bkalsow", "Brian", "Coleslaw", "abcde", 'u', false, false);
+		HashMap<String, User> users = uCont.viewUsers();
+		Assert.assertTrue("Users should match: ", users.get("bkalsow").equals(student));
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void editUserFailsForInvalidUserName()
+	{
+		uCont.editUser("", "Brian", "Coleslaw", "abcde", 'u', false, false);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void editUserFailsForInvalidFirstName()
+	{
+		uCont.editUser("bkalsow", "", "Coleslaw", "abcde", 'u', false, false);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void editUserFailsForInvalidLastName()
+	{
+		uCont.editUser("bkalsow", "Brian", "", "abcde", 'u', false, false);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void editUserFailsForInvalidPassword()
+	{
+		uCont.editUser("bkalsow", "Brian", "Coleslaw", "", 'u', false, false);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void editUserFailsForInvalidType()
+	{
+		uCont.editUser("bkalsow", "Brian", "Coleslaw", "abcde", ' ', false, false);
 	}
 
 }
