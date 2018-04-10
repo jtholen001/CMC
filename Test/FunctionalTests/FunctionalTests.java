7cb93daebac24fdc5e92a0127a9405434a13314a
NTAWIL001 <ntawil98@gmail.com>
2018-04-09 19:26:35 -0500

    created functionalTests.java

---
+++ Test/FunctionalTests/FunctionalTests.java
@@ -0,0 +1,54 @@
+package Test.FunctionalTests;
+
+import entityClasses.*;
+import Controllers.*;
+import Interfaces.*;
+
+/**
+ * @author Nick Tawil, Michael Carroll, Ryan Strelow, Jordan Tholen, Brandan Kalsow
+ * This class performs functional testing for the CMC system
+ * Each Use Case and every alternate scenario has exactly one test method
+ *
+ */
+public class FunctionalTests
+{
+	//TODO:U1
+	
+	//U2(ABSTRACT USE CASE)
+	
+	//TODO:U3
+	
+	//TODO:U4
+	
+	//TODO:U5
+	
+	//TODO:U6
+	
+	//TODO:U7
+	
+	//TODO:U8
+	
+	//TODO:U9
+	
+	//TODO:U10
+	
+	//TODO:U11
+	
+	//U12(ABSTRACT USE CASE)
+	
+	//TODO:U13
+	
+	//TODO:U14
+	
+	//TODO:U15
+	
+	//TODO:U16
+	
+	//TODO:U17
+	
+	//TODO:U18
+	
+	//TODO:U19
+	
+	//TODO:U20
+}
